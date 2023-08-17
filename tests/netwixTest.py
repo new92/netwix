aab09d0e1fd622a127b3059d66697d0e5e154aef
new92 <94779840+new92@users.noreply.github.com>
2023-08-17 23:16:50 +0300

    Update netwixTest.py

--- tests/netwixTest.py
+++ tests/netwixTest.py
@@ -1,4 +1,13 @@
-from netwix import __version__
+from netwix.__init__ import __version__
+from netwix.types import Movies, Series
 
 def testVers():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.2'
+
+def testMovies():
+    movie = Movies("81161626")
+    assert movie.name == 'Red Notice'
+
+def testSeries():
+    serie = Series("70126228")
+    assert serie.name == 'The Office'
