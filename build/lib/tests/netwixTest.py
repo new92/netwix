9df880bfa4f314029cedcafa7cf06227e8615c72
new92 <94779840+new92@users.noreply.github.com>
2023-09-15 13:34:04 +0300

    Add files via upload

---
+++ build/lib/tests/netwixTest.py
@@ -0,0 +1,13 @@
+from netwix.__init__ import __version__
+from netwix.types import Movies, Series
+
+def testVers():
+    assert __version__ == '0.1.6'
+
+def testMovies():
+    movie = Movies("81161626")
+    assert movie.name == 'Red Notice'
+
+def testSeries():
+    serie = Series("70126228")
+    assert serie.name == 'The Office'
